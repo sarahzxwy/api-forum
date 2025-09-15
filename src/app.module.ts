12b0dac7da8c38a10f6e8e8dbcc487e61c9fa11d
sarahzxwy <sarahalvesborges@outlook.com>
2025-09-15 17:26:57 -0300

    feat: auth guard, module and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { UsersModule } from './users/users.module';
 import { QuestionsModule } from './questions/questions.module';
 import { AnswersModule } from './answers/answers.module';
 import { DatabaseModule } from './database/database.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, QuestionsModule, AnswersModule, DatabaseModule],
+  imports: [UsersModule, QuestionsModule, AnswersModule, DatabaseModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
