773a2af7de4a8d03a39fbfb215524a32e5074ad4
sarahzxwy <sarahalvesborges@outlook.com>
2025-09-15 17:12:10 -0300

    feat: answers module and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { QuestionsModule } from './questions/questions.module';
+import { AnswersModule } from './answers/answers.module';
 
 @Module({
-  imports: [UsersModule, QuestionsModule],
+  imports: [UsersModule, QuestionsModule, AnswersModule],
   controllers: [AppController],
   providers: [AppService],
 })
