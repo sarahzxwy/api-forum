ce6e1822975bfd52d2f37ca832e93043c0160d4d
sarahzxwy <sarahalvesborges@outlook.com>
2025-09-16 16:18:53 -0300

    feat: validation rules to questions fields

--- src/questions/dto/create-question.dto.ts
+++ src/questions/dto/create-question.dto.ts
@@ -1,13 +1,13 @@
 import { IsString, IsNotEmpty, Length } from 'class-validator';
 
 export class CreateQuestionDto {
-//   @Length(6)
-//   @IsString()
-//   @IsNotEmpty()
+  @Length(6)
+  @IsString()
+  @IsNotEmpty()
   title: string;
 
-//   @Length(6)
-//   @IsString()
-//   @IsNotEmpty()
+  @Length(6)
+  @IsString()
+  @IsNotEmpty()
   body: string;
 }
