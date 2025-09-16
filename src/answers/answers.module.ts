80c32b77d88a10ed1ac70128e4ac8d6c8e8c75d0
sarahzxwy <sarahalvesborges@outlook.com>
2025-09-16 14:09:51 -0300

    feat: answers resource

--- src/answers/answers.module.ts
+++ src/answers/answers.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AnswersService } from './answers.service';
+import { AnswersController } from './answers.controller';
 
 @Module({
+  controllers: [AnswersController],
   providers: [AnswersService],
-  exports: [AnswersService]
 })
 export class AnswersModule {}
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,11 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { QuestionsModule } from './questions/questions.module';
 import { AnswersModule } from './answers/answers.module';
+import { UsersModule } from './users/users.module';
+import { AnswersModule } from './answers/answers.module';
 import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './auth/auth.module';
+import { QuestionsModule } from './questions/questions.module';
 
 @Module({
   imports: [UsersModule, QuestionsModule, AnswersModule, DatabaseModule, AuthModule],
