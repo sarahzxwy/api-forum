0e46772030c3888990f872375751d0662fab32f2
sarahzxwy <sarahalvesborges@outlook.com>
2025-09-16 15:43:13 -0300

    fix: ensure PrismaService connects on module initialization

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -18,24 +18,23 @@ export class UsersService {
         });
     }
 
-
-     async user(
+    async user(
         userWhereUniqueInput: Prisma.UserWhereUniqueInput,
     ): Promise<User | null> {
         return this.prisma.user.findUnique({
-        where: userWhereUniqueInput,
+            where: userWhereUniqueInput,
         });
     }
 
     async updateUser(params: {
-    where: Prisma.UserWhereUniqueInput;
-    data: Prisma.UserUpdateInput;
+        where: Prisma.UserWhereUniqueInput;
+        data: Prisma.UserUpdateInput;
     }): Promise<User> {
         const { where, data } = params;
         return this.prisma.user.update({ data, where });
-  }
+    }
 
-  async deleteUser(where: Prisma.UserWhereUniqueInput): Promise<User> {
-    return this.prisma.user.delete({ where });
-  }
+    async deleteUser(where: Prisma.UserWhereUniqueInput): Promise<User> {
+        return this.prisma.user.delete({ where });
+    }
 }
